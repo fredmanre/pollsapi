d6cbb7af5339551983847afeff9a5dd4ab9cd345
fredmanre <fredmanre@gmail.com>
2019-05-22 00:18:24 -0400

    api

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,11 +1,70 @@
 from rest_framework.views import APIView
+from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+
+# from django.shortcuts import get_object_or_404
 
 from .models import Poll, Choice
-from .serializers import PollSerializer
+from .serializers import (
+    PollSerializer,
+    ChoiceSerializer,
+    VoteSerializer
+)
 
 
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
+        return queryset
+
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(APIView):
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+"""
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+"""
+
+""" codigo APIVIew
 class PollList(APIView):
     def get(self, request):
         polls = Poll.objects.all()[:20]
@@ -18,3 +77,4 @@ class PollDetail(APIView):
         poll = get_object_or_404(Poll, pk=pk)
         data = PollSerializer(poll).data
         return Response(data)
+"""
--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 # from .views import polls_detail, polls_list
-from .apiviews import PollDetail, PollList
+from .apiviews import (
+    PollDetail,
+    PollList,
+    ChoiceList,
+    CreateVote,
+    PollViewSet,
+)
+
+
+router = DefaultRouter()
+router.register('polls', PollViewSet, base_name='polls')
 
 
 urlpatterns = [
@@ -8,4 +19,9 @@ urlpatterns = [
     # path("polls/<int:pk>/", polls_detail, name="polls_detail"),
     path("polls/", PollList.as_view(), name="polls_list"),
     path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+    # path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="polls_list"),
 ]
+
+urlpatterns += router.urls
